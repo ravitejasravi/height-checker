32723a8cb6062b8ced08197c93468cb8e6bf5800
Raviteja S <43201927+ravitejasravi@users.noreply.github.com>
2022-12-27 17:53:10 +0530

    Solution

---
+++ HeightChecker.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        
+        vector<int> temp = heights;
+
+        sort(temp.begin(), temp.end());
+        int index = 0;
+
+        for(int i = 0; i < heights.size(); i++)
+            if(temp[i] != heights[i])
+                index += 1;
+
+        return index;
+    }
+};
